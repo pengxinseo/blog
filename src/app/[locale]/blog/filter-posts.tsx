abba2d22ce106ce1ed2062baf13c8c46067af9aa
pengxinseo <208015671@qq.com>
2024-06-23 18:13:50 +0800

    1111

--- src/app/[locale]/blog/filter-posts.tsx
+++ src/app/[locale]/blog/filter-posts.tsx
@@ -24,7 +24,7 @@ const FilterPosts = ({
 	return (
 		<>
 			<div className='relative mt-4 animate-fade-in animation-delay-2'>
-				<RiSearch2Line className='absolute left-2 top-2.5 size-5 text-foreground/60' />
+				<RiSearch2Line className='absolute  left-2 top-2.5 size-5 text-foreground/60' />
 				<input
 					className=' w-full rounded-md border border-border bg-input/40 p-2 pl-8 text-sm outline-none ring-blue-500/20 duration-300 placeholder:text-foreground/80 focus:border-primary focus:ring-4 sm:w-[18.75rem]'
 					placeholder={placeholder}
