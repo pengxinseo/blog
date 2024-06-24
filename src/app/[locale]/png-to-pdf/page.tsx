45e27e42188bd4d5f5749218974b9a22fa8063be
pengxinseo <208015671@qq.com>
2024-06-24 09:21:12 +0800

    重新来111

--- src/app/[locale]/png-to-pdf/page.tsx
+++ src/app/[locale]/png-to-pdf/page.tsx
@@ -1,13 +1,9 @@
 import PageComponent from "./PageComponent";
 import { queryPostBySlug } from '../../../../db/queries';
 
-
-export const runtime = 'edge';
 export default async function IndexPage({params: {locale = ''}}) {
 
   const post = await queryPostBySlug('advanced-techniques-for-cms-security');
-
-
 
   return (
     <PageComponent
