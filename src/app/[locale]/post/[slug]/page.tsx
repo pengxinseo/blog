e29c8643705ec3e943e0879e0be5f7dc82187ebb
pengxinseo <208015671@qq.com>
2024-06-24 08:26:03 +0800

    牛逼

--- src/app/[locale]/post/[slug]/page.tsx
+++ src/app/[locale]/post/[slug]/page.tsx
@@ -1,32 +1,33 @@
 export const runtime = 'edge';
 import { notFound } from 'next/navigation';
-import { queryAllPosts,queryPostBySlug  } from '../../../../../db/queries';
+import { queryAllPosts, queryPostBySlug } from '../../../../../db/queries';
 import MdxRenderer from '@/components/MdxRenderer';
 
 type Props = {
   params: { slug: string };
 }
 
+
 export async function generateStaticParams() {
-  return queryAllPosts();
+  const posts = queryAllPosts();
+  return posts.map(post => ({ params: { slug: post.slug } }));
 }
 
-export default async function Post(props: Readonly<Props>) {
-  const { params: { slug } } = props
-  const post = await queryPostBySlug(slug);
-  const dateFormatter = new Intl.DateTimeFormat("en-US", { dateStyle: "full" })
+export default async function Post({ params: { slug } }: Readonly<Props>) {
+  const post = queryPostBySlug(slug);
+  const dateFormatter = new Intl.DateTimeFormat("en-US", { dateStyle: "full" });
 
   if (!post) {
-    notFound()
+    notFound();
   }
 
   return (
     <div className='p-32'>
       <time className="block italic text-gray-500">{dateFormatter.format(new Date(post.date))}</time>
       <h1 className="text-4xl font-bold pb-6 border-b mb-10">
-        {post.title} 
+        {post.title}
       </h1>
       <MdxRenderer source={post.content}></MdxRenderer>
     </div>
-  )
+  );
 }
